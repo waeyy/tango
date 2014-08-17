fe2cdf9a64522602ac237c49730697e669f4e4d0
SOIDIKI Djamil <soidikid@gmail.com>
2014-08-17 21:43:39 +0200

    Ajout partie Admin (Rewrite; backend.php) Signed-off-by: SOIDIKI Djamil <soidikid@gmail.com>

---
+++ web/backend.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * This file is part of the Symplicity Web Application Package.
+ *
+ * 17/08/14 21:11
+ * (c) Djamil SOIDIKI <soidikid@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+echo '<h1>ADMIN PAGE</h1>';
